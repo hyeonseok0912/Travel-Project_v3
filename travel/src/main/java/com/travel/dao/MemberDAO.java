9179020e371114816bc5e2cddd2795798c3cb7f7
user <user@DESKTOP-ERR5D88>
2024-01-30 18:00:09 +0900

    basic login function added

---
+++ travel/src/main/java/com/travel/dao/MemberDAO.java
@@ -0,0 +1,39 @@
+package com.travel.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.travel.dto.MemberDTO;
+
+public class MemberDAO extends AbstractDAO{
+
+	//로그인 메소드
+	public MemberDTO login(MemberDTO dto) {
+		Connection con = db.getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "SELECT count(*) as count, mname FROM tmember WHERE mid=? AND mpw=?";
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, dto.getMid());
+			pstmt.setString(2, dto.getMpw());
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dto.setCount(rs.getInt("count"));
+				dto.setMname(rs.getString("mname"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rs, pstmt, con);
+		}
+		
+		return dto;
+	}
+
+}
--- travel/src/main/java/com/travel/web/Login.java
+++ travel/src/main/java/com/travel/web/Login.java
@@ -1,11 +1,17 @@
 package com.travel.web;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.travel.dao.MemberDAO;
+import com.travel.dto.MemberDTO;
 
 @WebServlet("/login")
 public class Login extends HttpServlet {
@@ -16,11 +22,27 @@ public class Login extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		RequestDispatcher rd = request.getRequestDispatcher("/login.jsp");
+		rd.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		MemberDTO dto = new MemberDTO();
+		HttpSession session = request.getSession();
+		dto.setMid(request.getParameter("id"));
+		dto.setMpw(request.getParameter("pw"));
 		
+		MemberDAO dao = new MemberDAO();
+		dto = dao.login(dto);
+		
+		if(dto.getCount() == 1) {
+			session.setAttribute("mname", dto.getMname());
+			session.setAttribute("mid", dto.getMid());
+			
+			response.sendRedirect("./index");
+		}else {
+			response.sendRedirect("./login?error=000");
+		}
 	}
 
 }
